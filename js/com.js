a7e00260bbf1ddb2a544952f9647462350fcb1e7
jieunyoung <tnwl05200@gmail.com>
2021-10-25 18:57:18 +0900

    up

---
+++ js/com.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 2000,
+    },
+    speed: 2000,
+    effect: "fade"
+  });
