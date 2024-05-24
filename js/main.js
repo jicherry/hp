a0e542b89977f51ffff71a583c484831857ae4ce
chaebeen <tnwl05200@gmail.com>
2024-05-24 20:30:52 +0900

    수정

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 2000,
+    },
+    speed: 2000,
+    effect: "fade"
+  });
+
+jQuery(".player").YTPlayer();
+
+// $(".favorite_pink img").mouseover(function() {
+//   var src = $(this).attr("src");
+
+//   if(src == "./images/pink_back.jpg") {
+//     $(this).attr("src", "./images/pink.jpg");
+//   } else {
+//     $(this).attr("src", "./images/pink_back.jpg");
+//   }
+// });
---
+++ js/plete.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.swiper-container', {
+    autoplay: {
+        delay: 2000,
+    },
+    speed: 2000,
+    effect: "fade"
+  });
+
