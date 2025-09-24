fc17c96120acb8e014a1282d78f9df452df15141
chaebeen <tnwl05200@gmail.com>
2025-09-24 10:17:58 +0900

    modify

--- js/serum.js
+++ js/serum.js
@@ -1,13 +1,20 @@
-var swiper = new Swiper('.swiper-container', {
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+let swiper = new Swiper(".swiper-container", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 2000,
+  },
+  speed: 2000,
+  slidesPerView: 4,
+  spaceBetween: 10,
+  breakpoints: {
+    768: {
+      slidesPerView: 4,
     },
-    autoplay: {
-        delay: 2000,
+    0: {
+      slidesPerView: 2,
     },
-    speed: 2000,
-    slidesPerView: 4,
-    // effect: "fade"
-  });
-
+  },
+});
